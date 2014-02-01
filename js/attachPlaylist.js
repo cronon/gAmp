f710931b74fa2dda07458d64bc19d515f1fa0e76
cr <cronon12@gmail.com>
2014-02-01 18:02:11 +0300

    final commit

---
+++ js/attachPlaylist.js
@@ -0,0 +1,116 @@
+/**
+ * Attach playlist to view
+ */
+var attachPlaylist = function(playlist) {
+  var was = 0;
+
+  $('#playlist').sortable({ 
+      handle: ".handle", 
+      axis: 'y',
+      start: function(e,ui){
+          was = ui.item.index(); 
+      },
+      update: function(e,ui){
+          playlist.replaceSong(was,ui.item.index());
+      }
+    }).
+    selectable({ 
+      filter: "li", 
+      cancel: ".handle"
+    });
+
+  var onSongDblClick = function(e){
+    var i = $(e.target).index();
+    playlist.setCurrent(i);
+    controls.audio.src = playlist.getCurrentSong().src;
+    controls.play();
+  }
+
+  var onSongClick = function(e){
+    $('#playlist > li').removeClass('ui-selected');
+    $(this).addClass('ui-selected');
+  }
+
+  var onSongContext = function(e){
+    var s = '';
+    e.preventDefault();
+    var song = playlist[$(e.target).index()];
+    if (song.tags){
+      s+='Title: '+(song.tags.title||'')+'<br>';
+      s+='Artist: '+(song.tags.artist||'')+'<br>';
+      s+='Album: '+(song.tags.album || '')+'<br>';
+      s+='Year: '+(song.tags.year || '')+'<br>';      
+    }
+    s+='Duration: '+(song.duration || '');
+    $('#dialog').html(s).dialog({title:song.name}).dialog('open');
+  }
+
+  playlist.onSongsAdded = function(songs){
+    var s = '';
+    songs.forEach(function(song){
+      s = song.name;
+      $('#playlist').append("<li>" + s + "</li>");
+      $('li').last().addClass('not-highlited').
+                    dblclick(onSongDblClick).
+                    bind('contextmenu',onSongContext).
+                    append("<span class='handle ui-icon ui-icon-carat-2-n-s'></span>").
+                    append("<span class='duration'></span>");
+    });
+  }
+
+  var scrollToSong = function(id){
+    var position = $('li:eq('+id+')')[0].getBoundingClientRect()
+    var visible = document.elementFromPoint(position.left,position.top);
+    if(!visible || visible.nodeName.toLowerCase() != 'li'){
+      $('#playlist_wrapper')[0].scrollTop=$('li:eq('+id+')')[0].offsetTop;
+    }
+  }
+
+  /**
+   * Convert seconds to hh:mm:ss or mm:ss if hh=='00'
+   *
+   * @param {Number} seconds
+   * @return {String} String in hh:mm:ss or mm:ss format.
+   */
+  var secondsToTime = function(seconds){
+    var prependZero = function(x){
+      return String(x).length == 1 ? '0'+x : x
+    }
+    var h = Math.floor(seconds/3600);
+    var m = Math.floor(seconds/60) - h*60;
+    var s = Math.floor(seconds % 60);
+    return result = (h==0 ? '' : prependZero(h)+':') + prependZero(m) + ':' + prependZero(s);
+  }
+
+  playlist.onSongChanged = function(prev,next){
+    song = playlist[next];
+
+    var s = '';
+    if (song.tags && (song.tags['title'] || song.tags['artist'])){
+        s += song.tags['artist'] + ' - ' + song.tags['title'];
+    } else {
+        s += song.name;
+    }
+
+    if(!song.duration){
+      $(controls.audio).bind('canplay',function(e){
+        song.duration = secondsToTime(controls.audio.duration);
+        $('li:eq('+next+') > .duration').text(song.duration);
+        $(controls.audio).unbind('canplay');
+      });
+    }
+    
+    $('li:eq('+next+')').addClass('highlited').removeClass('not-highlited').
+        contents().first()[0].textContent=s;
+    title = s+'\u2007\u2007';
+    $('#current_song').text(s);
+    scrollToSong(next);
+    $('li:eq('+prev+')').addClass('not-highlited').removeClass('highlited');
+  }
+
+  playlist.onSongsRemoved = function(indexes){
+    indexes.forEach(function(i){
+      $('li:eq('+i+')').remove();
+    });
+  }
+};
---
+++ js/playlist.js
@@ -0,0 +1,93 @@
+var playlist = (function(){
+  var self = [];
+  var current = -1;
+
+  self.shuffle = false;
+  self.repeat = false;
+
+  /**
+   * @param {number} previous Index of previous song.
+   * @param {number} next Index of next song.
+   */
+  self.onSongChanged = function(previous, next){};
+
+  /**
+   * @param {Array} songs Array of songs.
+   */
+  self.onSongsAdded = function(songs){};
+
+  /**
+   * @param {Array} indexes of removed songs.
+   */
+  self.onSongsRemoved = function(indexes){};
+
+  self.getCurrentSong = function(){
+    return self[current];
+  }
+
+  self.setCurrent = function(i){
+    var prev = current;
+    current = i;
+    self.onSongChanged(prev,current);
+  }
+
+  self.next = function(){
+    var prev = current;
+    if(!self.repeat){current+=1};
+    if(self.shuffle){current = Math.floor(Math.random()*self.length % self.length);}
+    if(current>=self.length){
+      current -=self.length;
+    }
+    var next = current;
+    self.onSongChanged(prev,next);
+  }
+
+  self.previous = function(){
+    var prev = current;
+    current-=1;
+    if(current<0){
+      current=self.length-1;
+    }
+    var next = current;
+    self.onSongChanged(prev,next);
+  }
+
+  /**
+   * Add songs to playlist.
+   *
+   * @param {array} songs Array of songs.
+   */
+  self.addSongs = function(songs){
+    songs.forEach(function(song){
+      self.push(song);
+    });
+    self.onSongsAdded(songs);
+    if(current === -1){
+      self.setCurrent(0);
+    }
+  }
+
+  var spaceship = function(a,b){ // a <=> b
+    return a-b;
+  }
+  /**
+   * Removes songs from playlist.
+   *
+   * @param {array} indexes Array of songs' indexes.
+   */
+  self.removeSongs = function(indexes){
+    indexes.sort(spaceship).reverse().forEach(function(i){
+      (window.URL || window.webkitURL).revokeObjectURL(self[i].src);
+      self.splice(i,1);
+    });
+    self.onSongsRemoved(indexes);
+  }
+
+  self.replaceSong = function(was,become){
+    var current_song = self.getCurrentSong();
+    var song = self.splice(was,1)[0];
+    self.splice(become,0,song);    
+    current = self.indexOf(current_song);
+  }
+  return self;
+}());
